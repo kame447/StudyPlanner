d199c04b12c90629ea731b1c0d00d7ea344134c4
kame447 <76802107+kame447@users.noreply.github.com>
2026-07-22 14:45:58 +0900

    feat: 一時的な型bridgeを削除する

--- src/features/weeklyPlanning/semantic/weeklyPlanningFactGraphV2RecurrenceBridge.ts
+++
@@ -1,7 +0,0 @@
-import type { RecurrenceFact as BaseRecurrenceFact } from './weeklyPlanningFactGraph';
-
-export {};
-
-declare module './weeklyPlanningFactGraphV2' {
-  interface RecurrenceFact extends BaseRecurrenceFact {}
-}
